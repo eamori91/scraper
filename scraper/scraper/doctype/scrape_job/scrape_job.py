281aaafc5b0007a6e54853ce5ce665adcbd9a6da
eamori91 <ennioantoniomori@gmail.com>
2025-06-18 17:18:15 -0600

    feat: add basic doctypes and server scripts

---
+++ scraper/scraper/doctype/scrape_job/scrape_job.py
@@ -0,0 +1,13 @@
+"""DocType server script for Scrape Job."""
+from __future__ import annotations
+
+import frappe
+from frappe.model.document import Document
+
+
+class ScrapeJob(Document):
+    """Represents a scraping task."""
+
+    pass
+
+# EOF
